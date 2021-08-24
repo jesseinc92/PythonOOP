831032c21af66e8e7626b780955c0faad4ae11e6
Jesse Hill <jesse.o.hill@gmail.com>
2021-08-23 23:49:34 -0400

    finished exercise

--- serial.py
+++ serial.py
@@ -20,3 +20,24 @@ class SerialGenerator:
     100
     """
 
+    def __init__(self, start=0):
+        """Initialize a Serial Generator object with a starter and value 
+        attribute, which will be incremented."""
+
+        self.start = start
+        self.value = start
+
+    def __repr__(self):
+        return f'SerialGenerator(start={self.start}, value={self.value})'
+
+    def generate(self):
+        """Assign current value to a serial. Return that variable
+        and increment the value."""
+        serial = self.value
+        self.value += 1
+        return serial
+
+    def reset(self):
+        """Reset value to the original start value"""
+
+        self.value = self.start 
--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,35 @@
 """Word Finder: finds random words from a dictionary."""
-
+import random 
 
 class WordFinder:
-    ...
+    """Finds random words from a file passed by the user."""
+
+    def __init__(self, url):
+        self.url = url
+
+        word_file = open(url, 'r')
+
+        self.words = self.parse(open(self.url))
+
+    def __repr__(self):
+        return f'WordFinder(url={self.url})' 
+
+    def parse(self, word_file):
+        """Puts all words in a list"""
+
+        return [w.strip() for w in word_file]
+
+    def random(self):
+        """Prints out a random word from the list"""
+
+        return random.choice(self.words)
+
+
+
+
+class SpecialWordFinder(WordFinder):
+    """Returns a random word from a file, ignoring blank lines and comments"""
+
+    def random(self, word_file):
+        
+        return [w.strip() for w in word_file if w.strip() and not w.startswith('#')]
